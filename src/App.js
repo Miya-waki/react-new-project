ba1244029814fe543f275e87b19aa17b512febd6
Miya-waki <rahulgiri12@gmail.com>
2022-01-03 17:09:00 +0530

    m1

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 export default function app() {
+ let counter =100;
+  let username ="rohit";
+  let email ="rohit@gmail.com"
   return (
     <div>
-      <h1>hello world using atleast 2 tags</h1>
-      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab odit
-      reprehenderit quas accusamus laudantium totam eaque recusandae
-      eius,consequatur molestias illum, ratione officiis numquam amet.
+      <div>{counter}</div>
+    <div> {username}</div> 
+    <div>{email}</div>
     </div>
   );
 }
