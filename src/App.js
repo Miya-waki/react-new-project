042d271dec434c40d3133ef6e5d9dcf5b5e64a06
Miya-waki <rahulgiri12@gmail.com>
2022-01-03 16:28:03 +0530

    bc

--- src/App.js
+++ src/App.js
@@ -1,7 +1,10 @@
-export default function app(){
+export default function app() {
   return (
     <div>
-    <h1>hello world using atleast 2 tags</h1>
+      <h1>hello world using atleast 2 tags</h1>
+      Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab odit
+      reprehenderit quas accusamus laudantium totam eaque recusandae
+      eius,consequatur molestias illum, ratione officiis numquam amet.
     </div>
   );
 }
